bbe84b2dc723ec47b9c665d6ac3318afe8fb0d02
Steve Simpson <steve.simpson.ctr@dren.hpc.mil>
2015-04-03 13:07:38 -0400

    Initial commit of template code.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # yii2-chartjs
+
+*A [Yii2](http://www.yiiframework.com) extension for the [Chart.js](http://www.chartjs.org) library*
+
+## License
+
+yii-chartsjs is available under the [MIT license](http://opensource.org/licenses/MIT). 
---
+++ src/Chartjs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Chartjs Class file.
+ *
+ * @author Steve Simpson
+ * @link https://github.com/SteveSimpson/yii2-chartjs
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @version 1.0.2a
+ */
+
+namespace SteveSimpson\chartjs;
+
+use Yii;
+use yii\base\Widget;
+use yii\web\View;
+
+Class Chartjs extends Widget
+{
+    public function run()
+    {
+
+    }
+
+    protected function registerAssets()
+    {
+
+    }
+}
